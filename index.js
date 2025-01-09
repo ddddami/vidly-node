2549f44baf5c96fe81db31ce86e23fe7a8a8f8f8
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-01-09 11:05:16 +0100

    Add helmet

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const helmet = require("helmet");
 const genres = require("./routes/genres");
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
 app.use("/api/genres", genres);
 
 app.get("/", (req, res) => {
