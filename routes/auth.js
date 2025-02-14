a44cadc1a6afdb214d57f1afc5446ebc9293311d
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-02-14 19:22:43 +0100

    Generate jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const _ = require("lodash");
+require("dotenv").config();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const { User } = require("../models/users");
 
 const router = express.Router();
+
+async function generateToken(user) {
+	return jwt.sign(
+		{ id: user.id, name: user.name, email: user.email },
+		process.env.JWT_SECRET,
+		{
+			expiresIn: process.env.JWT_EXPIRES_IN,
+		},
+	);
+}
 
 router.post("/login", async (req, res) => {
 	const { error } = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	const user = await User.findOne({ email: req.body.email });
-	if (!user) return res.status(400).send("Invalid email or password.");
-
 	const validPassword = await bcrypt.compare(req.body.password, user.password);
 
-	if (!validPassword) return res.status(400).send("Invalid email or password.");
+	if (!user || !validPassword) {
+		return res.status(400).send("Invalid email or password.");
+	}
 
-	res.send(true);
+	const token = await generateToken(user);
+	res.json({ token });
 });
 
 const validate = (user) => {
