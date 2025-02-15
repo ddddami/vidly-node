7ef3ede8fefb0f5d4830a46ab06cfe0fc6ebd700
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-02-15 02:50:23 +0100

    fix: Error in login when user doesnt exist

--- routes/auth.js
+++ routes/auth.js
@@ -11,9 +11,13 @@ router.post("/login", async (req, res) => {
 	if (error) return res.status(400).send(error.details[0].message);
 
 	const user = await User.findOne({ email: req.body.email });
+
+	if (!user) {
+		return res.status(400).send("Invalid email or password.");
+	}
 	const validPassword = await bcrypt.compare(req.body.password, user.password);
 
-	if (!user || !validPassword) {
+	if (!validPassword) {
 		return res.status(400).send("Invalid email or password.");
 	}
 
