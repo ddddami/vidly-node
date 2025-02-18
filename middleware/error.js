68afc4cc0ee35d4fd9ecfc36be12f1947dbc6417
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-02-18 22:30:13 +0100

    Add logger in error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,20 @@
+const pino = require("pino");
+
 // err middleware. a special kind of mdw, should be called after every middleware
 module.exports = (err, req, res, next) => {
-  res.status(500).send("Something failed."); // log ex too
+  const logger = pino(
+    {
+      level: process.env.PINO_LOG_LEVEL || "info",
+      formatters: {
+        level: (label) => {
+          return { level: label.toUpperCase() };
+        },
+      },
+      timestamp: pino.stdTimeFunctions.isoTime,
+    },
+    pino.destination(`${__dirname}/logs/app.log`),
+  );
+
+  logger.error(err.message, err);
+  res.status(500).send("Something failed.");
 };
