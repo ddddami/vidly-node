f2c9864d52c1141fc73d6fe1fba60157f6869166
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-02-25 23:10:19 +0100

    refactor: Extract routes

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 require("express-async-errors");
 const express = require("express");
 const mongoose = require("mongoose");
-const helmet = require("helmet");
 const pino = require("pino")();
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const authMiddleware = require("./middleware/auth");
-const error = require("./middleware/error");
 
 require("dotenv").config();
 
 const app = express();
+
+require("./startup/routes")(app);
 
 process.on("uncaughtException", (ex) => {
 	console.log(ex);
@@ -35,17 +29,6 @@ mongoose
 	.connect("mongodb://localhost:27017/vidly")
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.error("Could not connect to MongoDB", err));
-
-app.use(express.json());
-app.use(helmet());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
 
 app.get("/api/protected", authMiddleware, (req, res) => {
 	res.json({ message: "This is a protected route", user: req.user });
