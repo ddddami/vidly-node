727db6321de2ee0db13ca4a8b7f20dd53a55da1a
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-02-28 23:56:34 +0100

    Populate the db tests/Genres

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,17 +1,28 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genres");
+
 let server;
 
+const testGenres = [{ name: "genre1" }, { name: "genre2" }];
+
 describe("/api/genres", () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     server = require("../../index");
+    // note: Each time test re-runs(watch), it inserts the genres into the db. as a best practice, afterEach, clean up the db/ whatever you did. (already in global setup)
+    await Genre.insertMany(testGenres);
   });
-  afterEach(() => {
+
+  afterEach(async () => {
     server.close();
   });
+
   describe("GET /", () => {
     it("should return all genres", async () => {
-      const response = await request(server).get("/api/genres");
-      expect(response.status).toBe(200);
+      const res = await request(server).get("/api/genres");
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      expect(res.body.length).toBe(2);
+      expect(res.body[0]).toHaveProperty("name");
     });
   });
 });
